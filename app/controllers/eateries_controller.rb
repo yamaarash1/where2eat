888ddc6e5899cb6efc8dc86095119bf43e42e60f
Tomonori Yamada <kuronekodai15@gmail.com>
2019-05-17 16:42:37 +0900

    fifth commit

--- app/controllers/eateries_controller.rb
+++ app/controllers/eateries_controller.rb
@@ -1,4 +1,17 @@
 class EateriesController < ApplicationController
   def new
   end
+
+  def create
+    eatery = Eatery.new(eatery_params)
+    eatery.save
+    flash[:success] = "#{eatery.name}を追加しました"
+    redirect_to root_path
+  end
+ 
+  private
+ 
+  def eatery_params
+    params.require(:eatery).permit(:name)
+  end
 end
